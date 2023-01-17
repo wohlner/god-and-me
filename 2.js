d08a1de61693884772a3b5e26f77f57d088956f6
Jen Wohlner <jennifer.wohlner@gmail.com>
2023-01-17 14:25:37 -0600

    page 3 initial commit and some tweaks

--- 2.js
+++ 2.js
@@ -11,22 +11,27 @@ function windowResized() {
 }
 
 function draw(){
-  textSize(width/36);
+  textSize(height/36);
   fill(4, 55, 112,255);
   textStyle(BOLD);
     
   textAlign(RIGHT, CENTER);
-  text("God loves me", width/2 + textWidth("me")/2,(height/21)*6);
+  text("God loves me", width/2 + textWidth("me")/2,(height/21)*5);
     
-  for (let i=7; i < 14; i++){
-      if (frameCount > i-6){
+  for (let i=6; i < 13; i++){
+      if (frameCount > i-5){
           text("me", width/2 + textWidth("me")/2,(height/21)*i);
       }
   } 
   
   if (frameCount >= 9){
       textAlign(LEFT, CENTER);
-      text("we love you",width/2 - textWidth("me")/2,(height/21)*14);
+      text("and",width/2 - textWidth("and ") - textWidth("me")/2,(height/21)*13);
+  }
+  
+  if (frameCount >= 10){
+      textAlign(LEFT, CENTER);
+      text("we love you too",width/2  - textWidth("me")/2,(height/21)*14);
   }
       
 }
